d3dcea050f7dc9ad0d52ac807e5313962df5e11c
Daniel P. Bullington <daniel.bullington@wellengineered.us>
2022-01-22 00:20:26 -0600

    Two (2) files missing from last commit.

---
+++ src/WellEngineered.Solder.Configuration/IConfigurable.cs
@@ -0,0 +1,20 @@
+﻿/*
+	Copyright ©2020-2021 WellEngineered.us, all rights reserved.
+	Distributed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+*/
+
+namespace WellEngineered.Solder.Configuration
+{
+	public interface IConfigurable
+	{
+		#region Properties/Indexers/Events
+
+		ISolderConfiguration Configuration
+		{
+			get;
+			set;
+		}
+
+		#endregion
+	}
+}
